62bc4cfdb8eb2b242f6fd734cbe7fecf0d65dc5a
zoubo0502 <zoubo0502@qq.com>
2019-01-20 12:42:22 +0800

    runnable start project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'user-detail', loadChildren: './user-detail/user-detail.module#UserDetailPageModule' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
