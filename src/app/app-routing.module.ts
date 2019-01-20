d02bb651b580bc5b620c743dc4e9e09d506b16b0
zoubo0502 <zoubo0502@qq.com>
2019-01-20 12:56:53 +0800

    delelte redundent component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'user-detail', loadChildren: './user-detail/user-detail.module#UserDetailPageModule' }
+  { path: "", loadChildren: "./tabs/tabs.module#TabsPageModule" }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
